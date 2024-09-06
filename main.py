9377d9c53de157bc5c05e85bb037464001a053cc
arpit <42350280+GlaucousGlaucus@users.noreply.github.com.>
2024-09-06 17:12:11 +0530

    basic account system setup

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sqlite3
-from flask import Flask, render_template
+
+from flask import Flask, flash, redirect, render_template, url_for
+
+from forms import LoginForm, RegistrationForm
 
 app = Flask(__name__)
-
-
+app.config['SECRET_KEY'] = '13ceb0bdfde20b0c64765791628ba245'
+# db = SQLAlchemy(app)
 dashboard_info = {"balance": "50"}
 
 
@@ -30,6 +33,11 @@ def get_sellers():
     return sellers
 
 
+@app.route('/layout')
+def layout():
+    return render_template("layout.html")
+
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -46,9 +54,30 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/layout')
-def layout():
-    return render_template("layout.html")
+@app.route('/account')
+def account():
+    return render_template("account.html")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == 'test' and form.password.data == '123':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == "__main__":
