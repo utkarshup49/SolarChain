dda9164b798ef5b0e15518afdc7abf4224e78279
Arpit <42350280+GlaucousGlaucus@users.noreply.github.com>
2024-12-02 07:52:12 +0530

    asset creation

---
+++ playground/account_constants.py
@@ -0,0 +1,36 @@
+import re
+
+from algosdk import mnemonic, account
+
+from playground.algo_transaction import mnemonic_phrase
+
+ACCOUNTS = [
+    # Local net
+    "ordinary trick boost jar hand census comfort again lamp multiply rude material wet garlic banner beef image champion resemble cloud runway apple gas absorb reason",
+    "eager suit quarter income spider goat web present talent sail dutch method spike welcome skate between soldier reveal ethics hover like october obvious absent raccoon",
+    "blush tornado delay draw slide item little fame pig bind banner tomato person deposit expire board clap range comic panel sand human verify about dust",
+
+    # Test Net
+    "about speed cupboard cushion middle catalog twenty mango nest produce tortoise orient shoe obvious gloom half fortune pride uncover member divert today dad abandon hire",
+    "surge doctor duck usage clog congress claw number lucky cannon label illness limit business volcano peace result member tribe wagon vendor senior crane about job"
+]
+
+class Account:
+
+    def __init__(self, account_mnemonic_phrase: str):
+        super().__init__()
+        self._mnemonic_phrase: str = account_mnemonic_phrase
+        self._private_key = mnemonic.to_private_key(account_mnemonic_phrase)
+        self._public_address: str = account.address_from_private_key(self._private_key)
+
+    @property
+    def private_key(self) -> str:
+        return self._private_key
+
+    @property
+    def mnemonic(self):
+        return self._mnemonic_phrase
+
+    @property
+    def address(self):
+        return self._public_address
---
+++ playground/asset_creation.py
@@ -0,0 +1,99 @@
+from algosdk import account, mnemonic
+from algosdk.transaction import SuggestedParams, PaymentTxn, SignedTransaction, AssetConfigTxn
+from algosdk.v2client import algod
+from algosdk.v2client.algod import AlgodClient
+from algosdk import transaction
+
+from playground.account_constants import ACCOUNTS, Account
+from playground.algo_try import address
+
+# LocalNet configuration
+TOKEN = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"  # Default AlgoKit LocalNet token
+SERVER_ADDRESS = "http://localhost:4001"  # Default AlgoKit LocalNet endpoint
+
+# Initialize the Algod client
+algod_client: AlgodClient = AlgodClient(TOKEN, SERVER_ADDRESS)
+print("Algod Client Created")
+
+# Check the connection by fetching the node status
+try:
+    status = algod_client.status()
+    print("Connected to Algorand Network.")
+    print("Network Status:", status)
+except Exception as e:
+    print("Failed to connect:", e)
+
+# Generate address and mnemonic if needed
+acc1 = Account(ACCOUNTS[0])
+acc2 = Account(ACCOUNTS[1])
+
+print("Public Address:", acc1.address)
+print("My address:", acc1.private_key)
+print("My mnemonic:", acc1.mnemonic)
+
+# amount = 1 * 1000000  # Amount in microAlgos (0.1 ALGO)
+
+params: SuggestedParams = algod_client.suggested_params()
+
+def create_asset():
+    # Create the transaction
+    txn = transaction.AssetConfigTxn(
+        sender=acc1.address,
+        sp=params,
+        default_frozen=False,
+        unit_name="units",
+        asset_name="Electrical Units",
+        manager=acc1.address,
+        reserve=acc1.address,
+        freeze=acc1.address,
+        clawback=acc1.address,
+        url="https://path/to/my/asset/details",
+        total=1000,
+        decimals=0,
+    )
+
+    # Sign the transaction with the private key
+    signed_txn: SignedTransaction = txn.sign(acc1.private_key)
+
+    # Send the transaction
+    tx_id: str = algod_client.send_transaction(signed_txn)
+    print(f"Sent asset create transaction with txid: {tx_id}")
+    # Wait for the transaction to be confirmed
+    results = transaction.wait_for_confirmation(algod_client, tx_id, 4)
+    print(f"Result confirmed in round: {results['confirmed-round']}")
+
+    # grab the asset id for the asset we just created
+    created_asset = results["asset-index"]
+    print(f"Asset ID created: {created_asset}")
+
+created_asset = 1016
+
+def receive_asset():
+    optin_txn = transaction.AssetOptInTxn(
+        sender=acc2.address, sp=params, index=created_asset
+    )
+    signed_optin_txn = optin_txn.sign(acc2.private_key)
+    txid = algod_client.send_transaction(signed_optin_txn)
+    print(f"Sent opt in transaction with txid: {txid}")
+
+    # Wait for the transaction to be confirmed
+    results = transaction.wait_for_confirmation(algod_client, txid, 4)
+    print(f"Result confirmed in round: {results['confirmed-round']}")
+
+def transfer_asset():
+    # Create transfer transaction
+    xfer_txn = transaction.AssetTransferTxn(
+        sender=acc1.address,
+        sp=params,
+        receiver=acc2.address,
+        amt=1,
+        index=created_asset,
+    )
+    signed_xfer_txn = xfer_txn.sign(acc1.private_key)
+    txid = algod_client.send_transaction(signed_xfer_txn)
+    print(f"Sent transfer transaction with txid: {txid}")
+
+    results = transaction.wait_for_confirmation(algod_client, txid, 4)
+    print(f"Result confirmed in round: {results['confirmed-round']}")
+
+transfer_asset()
